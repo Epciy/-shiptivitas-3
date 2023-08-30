0ffc4e8887f78e1a4a72887682c9759bf2208df0
Epciy <amountii00@gmail.com>
2023-08-30 14:19:14 +0200

    task3

--- answer.sql
+++ answer.sql
@@ -2,11 +2,32 @@
 
 -- PART 1: Create a SQL query that maps out the daily average users before and after the feature change
 
+WITH UserLoginDates AS (
+    SELECT
+        user_id,
+        DATE(login_timestamp) AS login_date,
+        COUNT(DISTINCT DATE(login_timestamp)) OVER (PARTITION BY user_id) AS total_active_days
+    FROM login_history
+)
+SELECT
+    login_date,
+    AVG(total_active_days) AS daily_avg_active_users
+FROM UserLoginDates
+GROUP BY login_date
+ORDER BY login_date;
 
 
 
 -- PART 2: Create a SQL query that indicates the number of status changes by card
 
+SELECT
+    c.id AS card_id,
+    c.name AS card_name,
+    COUNT(*) AS status_change_count
+FROM card_change_history ch
+JOIN card c ON ch.cardID = c.id
+GROUP BY c.id, c.name
+ORDER BY c.id;
 
 
 
---
+++ hypothetical_example.md
@@ -0,0 +1,21 @@
+# Hypothesis 1
+
+Enhance collaboration by allowing users to create shared task lists
+
+## Expected Impact
+
+Increase user engagement with shared tasks
+
+## Explanation
+
+Users can create task lists that can be shared with collaborators, promoting teamwork and increasing platform usage.
+
+
+# Hpothesis 2
+Implement a "Recommended Connections" feature to connect users with similar interests
+
+## Expected Impact 
+Increase in connections between users
+
+## Explanation
+The system analyzes user activity and suggests connections based on shared interests, fostering a sense of community.
